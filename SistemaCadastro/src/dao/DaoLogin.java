9708722a10c3150f4859d5da109e985872e13a0f
Fernando Gomes <fernando.eng.comp2014@gmail.com>
2020-09-18 14:35:47 -0300

    Criado a classe login

---
+++ SistemaCadastro/src/dao/DaoLogin.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import connection.SingleConnection;
+
+public class DaoLogin {
+
+	private Connection connection;
+	
+	public DaoLogin() {
+		connection = SingleConnection.getConnection();
+	}
+	
+	public boolean validar(String login, String senha) throws Exception{
+		String sql = "select * from usuario where login = '" + login + "' and senha = '" + senha + "'";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		if(resultSet.next()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
